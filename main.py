1d7a92b6e9e8d934aa1ee3818a878113bf92fb06
TiveCS <creativecode.id@gmail.com>
2022-05-17 23:17:58 +0700

    add fuzzification, inference, defuzzification structure

--- main.py
+++ main.py
@@ -17,5 +17,37 @@ def write_luaran(self):
     return
 
 
+# Attribute Grouping:
+# Rating:
+#   - Enak              : > 3.7
+#   - Biasa             : 2 - 4
+#   - Kurang Enak       : < 2.3
+#
+# Rata-rata Harga:
+#   - Mahal             : > 50 ribu
+#   - Dapat Diterima    : 15 - 55 ribu
+#   - Kurang Enak       : < 20 ribu
+#
+# Jumlah Menu:
+#   - Sangat Variatif   : > 17 menu
+#   - Variatif          : > 5 - 20 menu
+#   - Kurang Variatif   : < 7
+
+# TODO fungsi fuzzifikasi disini
+# contoh di slide halaman 54
+def fuzzification(self):
+    return
+
+# TODO fungsi inference disini
+# contoh di slide halaman 55-57
+def inference(self):
+    return
+
+# TODO fungsi defuzzifikasi disini
+# contoh di slide halaman 58
+def defuzzification(self):
+    return
+
+
 if __name__ == '__main__':
     print('hello')

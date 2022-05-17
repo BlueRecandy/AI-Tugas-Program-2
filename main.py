b9b6126636a8b2e8ece300239a2905defa77443b
TiveCS <creativecode.id@gmail.com>
2022-05-17 20:35:43 +0700

    rough I/O structure

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import openpyxl
 
+# Buat nyimpen data dari file masukan.xls
+data_masukan = {}
+
+# Buat nyimpen data yang nanti disimpan di luaran.xls
+data_luaran = {}
+
+
+# TODO Baca data dari file masukan.xls
+def read_masukan(self):
+    return
+
+
+# TODO Tulis data hasil olahan observasi ke file luaran.xls
+def write_luaran(self):
+    return
+
+
 if __name__ == '__main__':
     print('hello')

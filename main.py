0f54d138f7fe0af3823ce8c2a0f2b5701edbb2d9
TiveCS <creativecode.id@gmail.com>
2022-05-19 19:43:02 +0700

    add fuzzification

--- main.py
+++ main.py
@@ -60,20 +60,55 @@ def write_luaran():
 #   - Variatif          : > 5 - 35 menu
 #   - Kurang Variatif   : < 7
 
+rating_groups = [5, 4, 3.7, 2.3, 2, 0]
+menu_groups = [150, 35, 33, 7, 5, 1]
+harga_groups = [100000, 55000, 50000, 20000, 15000, 0]
+
+
+def get_grouping(value: float, groups: list[int]):
+    for i in range(0, len(groups)):
+        a = groups[i]
+        b = groups[i + 1]
+        lower = min(a, b)
+        upper = max(a, b)
+
+        if lower < value <= upper:
+            return lower, upper
+
+
+def calc_group(value: float, lower: float, upper: float):
+    low = -(value - upper) / (upper - lower)
+    high = (value - lower) / (upper - lower)
+    return low, high
+
 # TODO fungsi fuzzifikasi disini
 # contoh di slide halaman 54
 def fuzzification():
+    fuzzy_result = {}
+
     for resto_id in data_masukan.keys():
         resto_obj = data_masukan[resto_id]
 
         rating = resto_obj['rating']
-        jumlah_menu = resto_obj['jumlah_menu']
         harga_rata_rata = resto_obj['harga_rata_rata']
 
-        print('-' * 10)
-        # TODO Calculate attribute
-        print('-' * 10)
-    return
+        rating_lower, rating_upper = get_grouping(rating, rating_groups)
+        harga_lower, harga_upper = get_grouping(harga_rata_rata, harga_groups)
+
+        rating_fuzzy = calc_group(rating, rating_lower, rating_upper)
+        harga_fuzzy = calc_group(harga_rata_rata, harga_lower, harga_upper)
+
+        rating_result = {'lower': rating_fuzzy[0], 'upper': rating_fuzzy[1]}
+        harga_result = {'lower': harga_fuzzy[0], 'upper': harga_fuzzy[1]}
+
+        result = {
+            'rating': rating_result,
+            'harga': harga_result
+        }
+
+        fuzzy_result[resto_id] = result
+
+    return fuzzy_result
 
 
 # TODO fungsi inference disini
@@ -90,4 +125,4 @@ def defuzzification(self):
 
 if __name__ == '__main__':
     read_masukan()
-    write_luaran()
+    print(fuzzification())
